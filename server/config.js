ef1150cf93e819b66355ccbde2cd292be90df6e3
Hector Morales <Hector_0.2@icloud.com>
2022-03-08 12:30:32 -0400

    🔧 changed env config for server

--- server/config.js
+++ server/config.js
@@ -4,20 +4,17 @@ import fs from "fs"
 
 process.env.NODE_ENV = process.env.NODE_ENV || "development"
 
-if (process.env.NODE_ENV === "development") {
+process.env.PORT = process.env.PORT || 3000
 
-    process.env.PORT = process.env.PORT || 3000
+process.env.MONGO_HOST = process.env.MONGO_HOST || "mongodb://localhost:27018/students-admin"
 
-    process.env.MONGO_HOST = process.env.MONGO_HOST || "mongodb://localhost:27018/students-admin"
+process.env.MONGO_USR = process.env.MONGO_USR || ""
 
-    process.env.MONGO_USR = process.env.MONGO_USR || ""
+process.env.MONGO_PASS = process.env.MONGO_PASS || ""
 
-    process.env.MONGO_PASS = process.env.MONGO_PASS || ""
+process.env.DB_NAME = process.env.DB_NAME || "students-admin"
 
-    process.env.DB_NAME = process.env.DB_NAME || "students-admin"
-
-    process.env.ORIGINS = process.env.ORIGINS || "*"
-}
+process.env.ORIGINS = process.env.ORIGINS || "*"
 
 if (!fs.existsSync("./uploads")) {
 
